4251faf95d9acaa52f11624e480ea3a951af9652
blVck49 <adebiyitope49@gmail.com>
2021-08-18 12:46:56 +0100

    test added

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcom <a href="https://nextjs.org">TopsyHub</a>
+          Welcomes <a href="https://nextjs.org">TopsyHub</a>
         </h1>
       </main>
     </div>
