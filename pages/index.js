1e9b457fb39bfa196e2dd552602beba76f8f8f9b
osita <67206380+Osiclin@users.noreply.github.com>
2021-08-19 07:26:23 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome <a href="https://nextjs.org">TopsGS</a>
+          We welcome <a href="https://nextjs.org">TopsGS</a>
         </h1>
       </main>
     </div>
